017b6e9e98eb7ac16de62a2fbb041cc271fb5cc0
vienthongtaiphat <151997422+vienthongtaiphat@users.noreply.github.com>
2023-11-29 14:32:07 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'eaeqhpmwae' );
+define( 'DB_NAME', 'feyapngaee' );
 
 /** Database username */
-define( 'DB_USER', 'eaeqhpmwae' );
+define( 'DB_USER', 'feyapngaee' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'V7Yfk5ebNB' );
+define( 'DB_PASSWORD', '97GjnFQq2Z' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
